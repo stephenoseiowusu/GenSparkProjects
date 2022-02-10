043c1523905adedd8dfe1022f87d1602e1c7fa30
Steve <steveboy199@gmail.com>
2022-02-10 09:47:35 -0500

    first

--- src/DragonCave/DragonCave.java
+++ src/DragonCave/DragonCave.java
@@ -1,4 +1,9 @@
 package DragonCave;
 
 public class DragonCave {
+
+    String [] messages = {"You are in a land full of dragons. In front of you,",
+                          "you see two caves.In onve cave, the dragon if firendly",
+                          "and will share his treasure with you. The other dragon",
+                          "is greedy and hungry and will eat you on sight."};
 }
