88894c1cfc0fb20f148752ef5b574e9722595362
Steve <steveboy199@gmail.com>
2022-02-28 09:56:20 -0500

    first

--- src/HumanVsGoblins/GoblinTest.java
+++ src/HumanVsGoblins/GoblinTest.java
@@ -1,5 +1,6 @@
 package HumanVsGoblins;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoblinTest {
-
+    Goblin goblin;
     @BeforeEach
     void setUp() {
+        goblin = new Goblin(100,100);
     }
 
     @AfterEach
@@ -18,13 +20,17 @@ class GoblinTest {
 
     @Test
     void getPower() {
+        Assert.assertEquals(goblin.getPower(),100);
     }
 
     @Test
     void getHealth() {
+        Assert.assertEquals(goblin.getHealth(),100);
     }
 
     @Test
     void setHealth() {
+        goblin.setHealth(120);
+        Assert.assertEquals(goblin.getHealth(),120);
     }
 }
