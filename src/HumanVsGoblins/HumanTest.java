2df2b9ee8592ccbbc0bb793b0fe609067e0a23bc
Steve <steveboy199@gmail.com>
2022-02-28 09:54:53 -0500

    first

---
+++ src/HumanVsGoblins/HumanTest.java
@@ -0,0 +1,32 @@
+package HumanVsGoblins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumanTest {
+
+    @Test
+    void addHeath() {
+    }
+
+    @Test
+    void getHealth() {
+    }
+
+    @Test
+    void getCurrentWeaponInventory() {
+    }
+
+    @Test
+    void getCurrentPotionInventory() {
+    }
+
+    @Test
+    void getInventory() {
+    }
+
+    @Test
+    void setInventory() {
+    }
+}
